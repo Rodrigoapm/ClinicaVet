1444911da7c4b3a2301149b09700ae7f4181ad05
Rodrigoapm <rapmartins123>
2022-06-08 08:56:26 +0100

    Criação da tabela bd

--- app/src/main/java/com/example/clinicaveterinaria/TabelaBDClientes.kt
+++ app/src/main/java/com/example/clinicaveterinaria/TabelaBDClientes.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaBDClientes (db: SQLiteDatabase) : TabelaBD(db, NOME) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $CAMPO_CLIENTES_ID INTEGER NOT NULL, FOREIGN KEY ($CAMPO_CLIENTES_ID) REFERENCES ${TabelaBDClientes.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $CAMPO_CLIENTES_ID INTEGER NOT NULL, FOREIGN KEY ($CAMPO_CLIENTES_ID) REFERENCES ${TabelaBDClientes.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
