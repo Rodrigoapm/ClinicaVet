f33781c380836540ce212e64614f615616618f55
Rodrigoapm <rapmartins123>
2022-06-23 17:14:10 +0100

    ContentProvider

--- app/src/main/java/com/example/clinicaveterinaria/ContentProviderClinica.kt
+++ app/src/main/java/com/example/clinicaveterinaria/ContentProviderClinica.kt
@@ -8,3 +8,187 @@ import android.provider.BaseColumns
 class ContentProviderClinica : ContentProvider() {
     var dbOpenHelper: BDclinicaOpenHelper? = null
 
+    /**
+     * Implement this to initialize your content provider on startup.
+     * This method is called for all registered content providers on the
+     * application main thread at application launch time.  It must not perform
+     * lengthy operations, or application startup will be delayed.
+     *
+     *
+     * You should defer nontrivial initialization (such as opening,
+     * upgrading, and scanning databases) until the content provider is used
+     * (via [.query], [.insert], etc).  Deferred initialization
+     * keeps application startup fast, avoids unnecessary work if the provider
+     * turns out not to be needed, and stops database errors (such as a full
+     * disk) from halting application launch.
+     *
+     *
+     * If you use SQLite, [android.database.sqlite.SQLiteOpenHelper]
+     * is a helpful utility class that makes it easy to manage databases,
+     * and will automatically defer opening until first use.  If you do use
+     * SQLiteOpenHelper, make sure to avoid calling
+     * [android.database.sqlite.SQLiteOpenHelper.getReadableDatabase] or
+     * [android.database.sqlite.SQLiteOpenHelper.getWritableDatabase]
+     * from this method.  (Instead, override
+     * [android.database.sqlite.SQLiteOpenHelper.onOpen] to initialize the
+     * database when it is first opened.)
+     *
+     * @return true if the provider was successfully loaded, false otherwise
+     */
+    override fun onCreate(): Boolean {
+        dbOpenHelper = BDclinicaOpenHelper(context)
+        return true
+    }
+
+    /**
+     * Implement this to handle query requests from clients.
+     *
+     *
+     * Apps targeting [android.os.Build.VERSION_CODES.O] or higher should override
+     * [.query] and provide a stub
+     * implementation of this method.
+     *
+     *
+     * This method can be called from multiple threads, as described in
+     * [Processes
+ * and Threads]({@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
+     *
+     *
+     * Example client call:
+     *
+     *
+     * <pre>// Request a specific record.
+     * Cursor managedCursor = managedQuery(
+     * ContentUris.withAppendedId(Contacts.People.CONTENT_URI, 2),
+     * projection,    // Which columns to return.
+     * null,          // WHERE clause.
+     * null,          // WHERE clause value substitution
+     * People.NAME + " ASC");   // Sort order.</pre>
+     * Example implementation:
+     *
+     *
+     * <pre>// SQLiteQueryBuilder is a helper class that creates the
+     * // proper SQL syntax for us.
+     * SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
+     *
+     * // Set the table we're querying.
+     * qBuilder.setTables(DATABASE_TABLE_NAME);
+     *
+     * // If the query ends in a specific record number, we're
+     * // being asked for a specific record, so set the
+     * // WHERE clause in our query.
+     * if((URI_MATCHER.match(uri)) == SPECIFIC_MESSAGE){
+     * qBuilder.appendWhere("_id=" + uri.getPathLeafId());
+     * }
+     *
+     * // Make the query.
+     * Cursor c = qBuilder.query(mDb,
+     * projection,
+     * selection,
+     * selectionArgs,
+     * groupBy,
+     * having,
+     * sortOrder);
+     * c.setNotificationUri(getContext().getContentResolver(), uri);
+     * return c;</pre>
+     *
+     * @param uri The URI to query. This will be the full URI sent by the client;
+     * if the client is requesting a specific record, the URI will end in a record number
+     * that the implementation should parse and add to a WHERE or HAVING clause, specifying
+     * that _id value.
+     * @param projection The list of columns to put into the cursor. If
+     * `null` all columns are included.
+     * @param selection A selection criteria to apply when filtering rows.
+     * If `null` then all rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     * the values from selectionArgs, in order that they appear in the selection.
+     * The values will be bound as Strings.
+     * @param sortOrder How the rows in the cursor should be sorted.
+     * If `null` then the provider is free to define the sort order.
+     * @return a Cursor or `null`.
+     */
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+
+        val db = dbOpenHelper!!.readableDatabase
+
+        val colunas = projection as Array<String>
+        val selArgs = selectionArgs as Array<String>?
+
+        val id = uri.lastPathSegment
+
+        return when (getUriMatcher().match(uri)) {
+            URI_ANIMAL -> TabelaBDAnimal(db).query(colunas, selection, selArgs,null,null, sortOrder)
+            URI_CLIENTE -> TabelaBDCliente(db).query(colunas, selection, selArgs, null, null, sortOrder)
+            URI_ANIMAL_ESPECIFICO -> TabelaBDAnimal(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("$id"), null, null, null)
+            URI_CLIENTE_ESPECIFICO -> TabelaBDCliente(db).query(colunas,"${BaseColumns._ID}=?", arrayOf("$id"), null, null, null)
+            else -> null
+        }
+
+    }
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        val db = dbOpenHelper!!.writableDatabase
+
+        requireNotNull(values)
+
+        val id = when (getUriMatcher().match(uri)) {
+            URI_ANIMAL -> TabelaBDAnimal(db).insert(values)
+            URI_CLIENTES_ESPECIFICO -> TabelaBDCliente(db).insert(values)
+            else -> -1
+        }
+        db.close()
+
+        if (id == -1L) return null
+
+        return Uri.withAppendedPath(uri, "$id")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        val db = dbOpenHelper!!.writableDatabase
+
+
+        val id = uri.lastPathSegment
+
+        val registosApagados = when (getUriMatcher().match(uri)) {
+            URI_ANIMAL_ESPECIFICO -> TabelaBDAnimal(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
+            URI_CLIENTES_ESPECIFICO -> TabelaBDCliente(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
+            else -> 0
+        }
+        db.close()
+
+        return registosApagados
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+
+        requireNotNull(values)
+
+        val db = dbOpenHelper!!.writableDatabase
+
+        val id = uri.lastPathSegment
+
+        val registosAlterados = when (getUriMatcher().match(uri)) {
+            URI_ANIMAL_ESPECIFICO -> TabelaBDAnimal(db).update(values, "${BaseColumns._ID}=?", arrayOf("${id}"))
+            URI_ClienteS_ESPECIFICO -> TabelaBDCliente(db).update(values,"${BaseColumns._ID}=?", arrayOf("${id}"))
+            else -> 0
+        }
+
+        db.close()
+
+        return registosAlterados
+    }
+}
+
+
+
